09a8c917f9cf0fec022789f0be9743a6c6138612
xuberance137 <>
2018-09-03 00:21:32 -0700

    Cleanup

--- test_02.py
+++ test_02.py
@@ -1,17 +1,22 @@
 # http://www.umiacs.umd.edu/~jbg/docs/nips2009-rtl.pdf
 
 
-from cryptopanic import CRYPTOPANIC_API_AUTH_TOKEN
+from tokens import CRYPTOPANIC_API_AUTH_TOKEN
 import requests
+from pprint import pprint
 
 URL_BASE = 'https://cryptopanic.com'
-URL_ADD = '/api/posts/?auth_token='+CRYPTOPANIC_API_AUTH_TOKEN+'&currencies=EOS&filter=trending'
+URL_ADD = '/api/posts/?auth_token='+CRYPTOPANIC_API_AUTH_TOKEN+'&currencies=ETH&filter=trending'
 
-for number in range(3):
+print
+print 
+
+for number in range(2):
 	URL_PAGE_NUMBER='&page='+str(number+1)
 	URL = URL_BASE+URL_ADD+URL_PAGE_NUMBER
 	r = requests.get(URL)
-	# print(r.json())
+	pprint(r.json())
+	
 	print
 	content = r.json()['results']
 	for index, item in enumerate(content):
